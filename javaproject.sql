85543d31473a4be0a8637443a3e34cc8967ef58b
MURILO VITOR SANTANA GAMA <127545356+Murilovsgama@users.noreply.github.com>
2023-06-19 00:49:08 -0300

    Add files via upload

---
+++ javaproject.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id INT auto_increment,
+    name VARCHAR(42),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    address VARCHAR(200),
+    Password VARCHAR(100),
+    PRIMARY KEY (id)
+);
+
+
+
+select * from users;
+
+
+
